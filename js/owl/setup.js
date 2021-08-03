3c0d207421551f3d860ebd766a2aaf281b3884e2
jrjunior72 <jrjunior72@hotmail.com>
2021-08-03 11:14:18 -0300

    Primeiro carregamento da configuração.

---
+++ js/owl/setup.js
@@ -0,0 +1,37 @@
+/* $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+}) */
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:false,
+            loop:false
+        }
+    }
+})
